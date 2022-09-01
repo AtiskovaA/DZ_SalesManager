3eb97a5af4342f228ef3311d75a62b7a32e648c9
Atiskova Anastasia <mailbox.nk@mail.ru>
2022-09-01 08:54:07 +0300

    Слияние веток

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        SalesManager oSale = new SalesManager(new int[]{});;
+        SalesManager oSale = new SalesManager(new long[]{});;
         oSale.max();
 
         System.out.println(oSale.ASale());
